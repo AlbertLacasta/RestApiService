042576baafdb03aa0f21976f019cc7cce8b48a7b
Albert Lacasta <47864341+AlbertLacasta@users.noreply.github.com>
2020-11-24 21:27:55 +0100

    working on project structure and api configuration

---
+++ src/config.py
@@ -0,0 +1,4 @@
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql://albert:P@ssw0rdSQL1@10.0.10.50/sample"
